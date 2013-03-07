1b5a96f626229a3d629e7bfca5c182973866c2a2
cwinux@gmail.com <cwinux@gmail.com@139e6f35-fde5-aae7-7073-d4f4ae540fe1>
2013-03-07 14:09:54 +0000

    git-svn-id: http://dcmd.googlecode.com/svn/trunk@16 139e6f35-fde5-aae7-7073-d4f4ae540fe1

---
+++ src/agent/dcmd_agent_def.cc
@@ -0,0 +1,15 @@
+﻿#include "dcmd_agent_def.h"
+
+namespace dcmd {
+DcmdCenter::DcmdCenter(){
+  host_id_ = 0;
+  host_port_ = 0;
+  conn_id_ = 0;
+  last_heatbeat_time_ = 0;
+  heatbeat_internal_ = kDefHeatbeatSecond;
+  max_package_size_ = kDefMaxPackageMSize;
+  is_connected_ = false;
+  is_auth_ = false;
+  is_auth_failed_ = false;
+}
+}
