8bb236d6863210b6c1c75837abc6fba84dde016c
cwinux@gmail.com <cwinux@gmail.com@139e6f35-fde5-aae7-7073-d4f4ae540fe1>
2013-03-14 14:47:23 +0000

    git-svn-id: http://dcmd.googlecode.com/svn/trunk@45 139e6f35-fde5-aae7-7073-d4f4ae540fe1

--- src/cmn/dcmd_def.cc
+++ src/cmn/dcmd_def.cc
@@ -3,17 +3,6 @@
 #include <CwxMd5.h>
 #include <CwxFile.h>
 namespace dcmd {
-DcmdCenter::DcmdCenter(){
-  host_id_ = 0;
-  host_port_ = 0;
-  conn_id_ = 0;
-  last_heatbeat_time_ = 0;
-  heatbeat_internal_ = kDefHeatbeatSecond;
-  max_package_size_ = kDefMaxPackageMSize;
-  is_connected_ = false;
-  is_auth_ = false;
-  is_auth_failed_ = false;
-}
 
 bool dcmd_parse_host_port(string const& host_port, CwxHostInfo& host) {
   if ((host_port.find(':') == string::npos) || (0 == host_port.find(':')))
