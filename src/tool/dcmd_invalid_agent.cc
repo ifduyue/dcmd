5e6424d86e5e37f237b3e01e44116c5ba20c3025
cwinux@gmail.com <cwinux@gmail.com@139e6f35-fde5-aae7-7073-d4f4ae540fe1>
2013-06-23 20:56:34 +0000

    git-svn-id: http://dcmd.googlecode.com/svn/trunk@279 139e6f35-fde5-aae7-7073-d4f4ae540fe1

--- src/tool/dcmd_invalid_agent.cc
+++ src/tool/dcmd_invalid_agent.cc
@@ -156,9 +156,9 @@ int main(int argc ,char** argv) {
     for (int i=0; i<reply.agentinfo_size(); i++) {
       printf("*****************ip:%s********************\n", reply.agentinfo_size(i).ip().c_str());
       printf("\tstate:%d\n", reply.agentinfo(i).state());
-      printf("\tversion:%d\n", reply.agentinfo(i).version().c_str());
-      printf("\connected_ip:%d\n", reply.agentinfo(i).connected_ip().c_str());
-      printf("\reported_ip:%d\n", reply.agentinfo(i).reported_ip().c_str());
+      printf("\tversion:%s\n", reply.agentinfo(i).version().c_str());
+      printf("\tconnected_ip:%s\n", reply.agentinfo(i).connected_ip().c_str());
+      printf("\treported_ip:%s\n", reply.agentinfo(i).reported_ip().c_str());
     }
   }
   return 0;
