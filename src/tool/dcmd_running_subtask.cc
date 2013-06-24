4cbfe546f1f13c64de3294d79e890ba9261712fd
cwinux@gmail.com <cwinux@gmail.com@139e6f35-fde5-aae7-7073-d4f4ae540fe1>
2013-06-24 13:39:48 +0000

    git-svn-id: http://dcmd.googlecode.com/svn/trunk@286 139e6f35-fde5-aae7-7073-d4f4ae540fe1

--- src/tool/dcmd_running_subtask.cc
+++ src/tool/dcmd_running_subtask.cc
@@ -12,23 +12,25 @@ using namespace cwinux;
 string     g_host;
 uint16_t   g_port = 0;
 int        g_client_id = 0;
-string     g_subtask_ids;
+string     g_agent_ip;
+string     g_svr_name;
 string     g_user;
 string     g_passwd;
 ///-1：失败；0：help；1：成功
 int parse_arg(int argc, char**argv)
 {
-  CwxGetOpt cmd_option(argc, argv, "H:P:c:s:i:u:p:h");
+  CwxGetOpt cmd_option(argc, argv, "H:P:c:i:S:u:p:h");
   int option;
   while( (option = cmd_option.next()) != -1) {
     switch (option) {
     case 'h':
-      printf("Get subtask's process.\n");
-      printf("%s  -H host -P port -c client-id -s subtask_ids .....\n", argv[0]);
+      printf("Get agent's running subtasks.\n");
+      printf("%s  -H host -P port -c client-id -i agent-ip  .....\n", argv[0]);
       printf("-H: server host\n");
       printf("-P: server port\n");
       printf("-c: client id\n");
-      printf("-s: subtask_ids, split by [%c] for multi-subtask_id\n", dcmd::kItemSplitChar);
+      printf("-i: agent ip.\n");
+      printf("-S: service name.\n");
       printf("-u: user name.\n");
       printf("-p: user password.\n");
       printf("-h: help\n");
@@ -54,12 +56,19 @@ int parse_arg(int argc, char**argv)
       }
       g_client_id = strtoul(cmd_option.opt_arg(), NULL, 10);
       break;
-    case 's':
+    case 'i':
       if (!cmd_option.opt_arg() || (*cmd_option.opt_arg() == '-')) {
-        printf("-s requires an argument.\n");
+        printf("-i requires an argument.\n");
         return -1;
       }
-      g_subtask_ids = cmd_option.opt_arg();
+      g_agent_ip = cmd_option.opt_arg();
+      break;
+    case 'S':
+      if (!cmd_option.opt_arg() || (*cmd_option.opt_arg() == '-')) {
+        printf("-S requires an argument.\n");
+        return -1;
+      }
+      g_svr_name = cmd_option.opt_arg();
       break;
     case 'u':
       if (!cmd_option.opt_arg() || (*cmd_option.opt_arg() == '-')) {
@@ -99,8 +108,8 @@ int parse_arg(int argc, char**argv)
     printf("No port, set by -P\n");
     return -1;
   }
-  if (!g_subtask_ids.length()){
-    printf("No subtask id, set by -s\n");
+  if (!g_agent_ip.length()){
+    printf("No agent ip, set by -i\n");
     return -1;
   }
   return 1;
@@ -120,32 +129,24 @@ int main(int argc ,char** argv) {
   }
   CwxMsgBlock* block=NULL;
   string query_msg;
-  dcmd_api::UiAgentTaskProcess query;
+  dcmd_api::UiAgentRunningTask query;
 
   query.set_client_msg_id(g_client_id);
-  //add subtask id
-  {
-    list<string> subtask_ids;
-    CwxCommon::split(g_subtask_ids, subtask_ids,dcmd::kItemSplitChar);
-    list<string>::iterator iter = subtask_ids.begin();
-    while (iter != subtask_ids.end()) {
-      *query.add_subtask_id() = *iter;
-      ++iter;
-    }
-  } 
+  query.set_ip(g_agent_ip);
+  if (g_svr_name.length()) query.set_svr_name(g_svr_name);
   query.set_user(g_user);
   query.set_passwd(g_passwd);
   if (!query.SerializeToString(&query_msg)) {
     printf("Failure to serialize query-msg.\n");
     return 1;
   }
-  CwxMsgHead head(0, 0, dcmd_api::MTYPE_UI_SUBTASK_PROCESS, 0, query_msg.length());
+  CwxMsgHead head(0, 0, dcmd_api::MTYPE_UI_AGENT_RUNNING_SUBTASK, 0, query_msg.length());
   block = CwxMsgBlockAlloc::pack(head, query_msg.c_str(), query_msg.length());
   if (!block) {
-    printf("Failure to pack fetch-subtask-process msg.\n");
+    printf("Failure to pack fetch-agent-running-subtask msg.\n");
     return 1;
   }
-  if (block->length() != (CWX_UINT32)CwxSocket::write_n(stream.getHandle(),
+  if (block->length() != (uint32_t)CwxSocket::write_n(stream.getHandle(),
       block->rd_ptr(), block->length()))
   {
     CwxMsgBlockAlloc::free(block);
@@ -158,13 +159,13 @@ int main(int argc ,char** argv) {
     printf("failed to read the reply, errno=%d\n", errno);
     return 1;
   }
-  if (dcmd_api::MTYPE_UI_SUBTASK_PROCESS_R != head.getMsgType()) {
+  if (dcmd_api::MTYPE_UI_AGENT_RUNNING_SUBTASK_R != head.getMsgType()) {
     printf("receive a unknow msg type, msg_type=%u\n", head.getMsgType());
     if (block) CwxMsgBlockAlloc::free(block);
     return 1;
   }
   query_msg.assign(block->rd_ptr(), block->length());
-  dcmd_api::UiAgentTaskProcessReply reply;
+  dcmd_api::UiAgentRunningTaskReply reply;
   if (!reply.ParseFromString(query_msg)) {
     printf("failed to parse reply-msg\n");
     if (block) CwxMsgBlockAlloc::free(block);
@@ -176,11 +177,14 @@ int main(int argc ,char** argv) {
   if (dcmd_api::DCMD_STATE_SUCCESS != reply.state()) {
     printf("err:%s\n", reply.err().c_str());
   } else {
-    printf("subtask process:\n");
-    for (int i=0; i<reply.process_size(); i++) {
-      printf("subtask_id:%s|process:%s\n",
-        reply.process(i).subtask_id().c_str(),
-        reply.process(i).process().c_str());
+    printf("subtask info:\n");
+    for (int i=0; i<reply.result_size(); i++) {
+      printf("svr_name:%s|tasm_cmd:%s|task_id:%s|subtask_id:%s|cmd_id:%s\n",
+        reply.result(i).svr_name().c_str(),
+        reply.result(i).task_cmd().c_str(),
+        reply.result(i).task_id().c_str(),
+        reply.result(i).subtask_id().c_str(),
+        reply.result(i).cmd_id().c_str());
     }
   }
   return 0;
