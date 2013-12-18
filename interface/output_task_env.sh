4b367b48b72f4efaa71d6e22200a6e5690e8b91d
cwinux@gmail.com <cwinux@gmail.com@139e6f35-fde5-aae7-7073-d4f4ae540fe1>
2013-12-18 14:37:54 +0000

    git-svn-id: http://dcmd.googlecode.com/svn/trunk@349 139e6f35-fde5-aae7-7073-d4f4ae540fe1

--- interface/output_task_env.sh
+++ interface/output_task_env.sh
@@ -1,15 +1,15 @@
-export DCMD_SVR_POOL=
-export DCMD_SVR_NAME=
-export DCMD_SVR_VERSION=
-export DCMD_SVR_REPO=
+export DCMD_SVR_POOL=pool
+export DCMD_SVR_NAME=svr
+export DCMD_SVR_VERSION=v1.1
+export DCMD_SVR_REPO=http://dianshang.wanda.cn/svn/maintenance/tmp
 export DCMD_SVR_USER=
 export DCMD_SVR_IP=
-export DCMD_SVR_UPDATE_ENV=
-export DCMD_SVR_UPDATE_TAG=
+export DCMD_SVR_UPDATE_ENV=0
+export DCMD_SVR_UPDATE_TAG=0
 export DCMD_SVR_ENV_V=
-export DCMD_SVR_OUT_FILE=
+export DCMD_SVR_OUT_FILE=/home/diaowenbo/dcmd/output.log
 export DCMD_SVR_PROCESS=
 # following is task-cmd's arg
 export DCMD_TASK_xxx=
 export DCMD_TASK_yyy=
-
+dcmd_task_install.script
